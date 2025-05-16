843ac1d2bea5c8fbf4d6fe22514e680e86f26af6
huza <huzaifahasif3@gmail.com>
2025-05-16 23:24:27 +0700

    testing: added purchase details

--- drivethru/testing.Designer.vb
+++ drivethru/testing.Designer.vb
@@ -24,14 +24,32 @@ Partial Class testing
     Private Sub InitializeComponent()
         TabControl1 = New TabControl()
         TabPage1 = New TabPage()
+        Panel1 = New Panel()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
         TabPage2 = New TabPage()
         TabPage3 = New TabPage()
         TabPage4 = New TabPage()
-        Panel1 = New Panel()
-        TableLayoutPanel1 = New TableLayoutPanel()
+        ComboBox1 = New ComboBox()
+        Button13 = New Button()
+        Button14 = New Button()
+        ListView1 = New ListView()
+        Label1 = New Label()
         TabControl1.SuspendLayout()
         TabPage1.SuspendLayout()
         Panel1.SuspendLayout()
+        TableLayoutPanel1.SuspendLayout()
         SuspendLayout()
         ' 
         ' TabControl1
@@ -43,7 +61,7 @@ Partial Class testing
         TabControl1.Location = New Point(12, 11)
         TabControl1.Name = "TabControl1"
         TabControl1.SelectedIndex = 0
-        TabControl1.Size = New Size(400, 435)
+        TabControl1.Size = New Size(400, 471)
         TabControl1.TabIndex = 0
         ' 
         ' TabPage1
@@ -55,34 +73,9 @@ Partial Class testing
         TabPage1.Location = New Point(4, 34)
         TabPage1.Name = "TabPage1"
         TabPage1.Padding = New Padding(3)
-        TabPage1.Size = New Size(392, 397)
+        TabPage1.Size = New Size(392, 433)
         TabPage1.TabIndex = 0
         TabPage1.Text = "TabPage1"
-        ' 
-        ' TabPage2
-        ' 
-        TabPage2.Location = New Point(4, 34)
-        TabPage2.Name = "TabPage2"
-        TabPage2.Padding = New Padding(3)
-        TabPage2.Size = New Size(392, 397)
-        TabPage2.TabIndex = 1
-        TabPage2.Text = "TabPage2"
-        ' 
-        ' TabPage3
-        ' 
-        TabPage3.Location = New Point(4, 34)
-        TabPage3.Name = "TabPage3"
-        TabPage3.Size = New Size(392, 397)
-        TabPage3.TabIndex = 2
-        TabPage3.Text = "TabPage3"
-        ' 
-        ' TabPage4
-        ' 
-        TabPage4.Location = New Point(4, 34)
-        TabPage4.Name = "TabPage4"
-        TabPage4.Size = New Size(392, 397)
-        TabPage4.TabIndex = 3
-        TabPage4.Text = "TabPage4"
         ' 
         ' Panel1
         ' 
@@ -90,7 +83,7 @@ Partial Class testing
         Panel1.Controls.Add(TableLayoutPanel1)
         Panel1.Location = New Point(6, 6)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(380, 385)
+        Panel1.Size = New Size(380, 421)
         Panel1.TabIndex = 0
         ' 
         ' TableLayoutPanel1
@@ -98,6 +91,18 @@ Partial Class testing
         TableLayoutPanel1.ColumnCount = 2
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
         TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.Controls.Add(Button1, 0, 0)
+        TableLayoutPanel1.Controls.Add(Button2, 1, 0)
+        TableLayoutPanel1.Controls.Add(Button3, 0, 1)
+        TableLayoutPanel1.Controls.Add(Button4, 1, 1)
+        TableLayoutPanel1.Controls.Add(Button5, 0, 2)
+        TableLayoutPanel1.Controls.Add(Button6, 1, 2)
+        TableLayoutPanel1.Controls.Add(Button7, 0, 3)
+        TableLayoutPanel1.Controls.Add(Button8, 1, 3)
+        TableLayoutPanel1.Controls.Add(Button9, 0, 4)
+        TableLayoutPanel1.Controls.Add(Button10, 1, 4)
+        TableLayoutPanel1.Controls.Add(Button11, 0, 5)
+        TableLayoutPanel1.Controls.Add(Button12, 1, 5)
         TableLayoutPanel1.Location = New Point(3, 3)
         TableLayoutPanel1.Name = "TableLayoutPanel1"
         TableLayoutPanel1.RowCount = 6
@@ -110,20 +115,204 @@ Partial Class testing
         TableLayoutPanel1.Size = New Size(347, 807)
         TableLayoutPanel1.TabIndex = 0
         ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(3, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(167, 128)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(176, 3)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(168, 128)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(3, 137)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(167, 128)
+        Button3.TabIndex = 2
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(176, 137)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(168, 128)
+        Button4.TabIndex = 3
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(3, 271)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(167, 128)
+        Button5.TabIndex = 4
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(176, 271)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(168, 128)
+        Button6.TabIndex = 5
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(3, 405)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(167, 128)
+        Button7.TabIndex = 6
+        Button7.Text = "Button7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(176, 405)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(168, 128)
+        Button8.TabIndex = 7
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(3, 539)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(167, 128)
+        Button9.TabIndex = 8
+        Button9.Text = "Button9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(176, 539)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(168, 128)
+        Button10.TabIndex = 9
+        Button10.Text = "Button10"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(3, 673)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(167, 131)
+        Button11.TabIndex = 10
+        Button11.Text = "Button11"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(176, 673)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(168, 131)
+        Button12.TabIndex = 11
+        Button12.Text = "Button12"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Location = New Point(4, 34)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(392, 433)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "TabPage2"
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.Location = New Point(4, 34)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Size = New Size(392, 433)
+        TabPage3.TabIndex = 2
+        TabPage3.Text = "TabPage3"
+        ' 
+        ' TabPage4
+        ' 
+        TabPage4.Location = New Point(4, 34)
+        TabPage4.Name = "TabPage4"
+        TabPage4.Size = New Size(392, 433)
+        TabPage4.TabIndex = 3
+        TabPage4.Text = "TabPage4"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Tunai", "Kartu Kredit", "Kartu Debit", "Amba Card"})
+        ComboBox1.Location = New Point(733, 325)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(182, 33)
+        ComboBox1.TabIndex = 2
+        ' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(418, 364)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(497, 51)
+        Button13.TabIndex = 3
+        Button13.Text = "Button13"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(418, 421)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(497, 57)
+        Button14.TabIndex = 4
+        Button14.Text = "Button14"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Location = New Point(418, 117)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(497, 202)
+        ListView1.TabIndex = 5
+        ListView1.UseCompatibleStateImageBehavior = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(575, 328)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(152, 25)
+        Label1.TabIndex = 6
+        Label1.Text = "Payment Method:"
+        ' 
         ' testing
         ' 
         AutoScaleDimensions = New SizeF(144F, 144F)
         AutoScaleMode = AutoScaleMode.Dpi
-        ClientSize = New Size(927, 450)
+        ClientSize = New Size(927, 494)
+        Controls.Add(Label1)
+        Controls.Add(ListView1)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(ComboBox1)
         Controls.Add(TabControl1)
         FormBorderStyle = FormBorderStyle.FixedSingle
         Name = "testing"
         StartPosition = FormStartPosition.CenterScreen
-        Text = "testing"
+        Text = "h"
         TabControl1.ResumeLayout(False)
         TabPage1.ResumeLayout(False)
         Panel1.ResumeLayout(False)
+        TableLayoutPanel1.ResumeLayout(False)
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents TabControl1 As TabControl
@@ -133,4 +322,21 @@ Partial Class testing
     Friend WithEvents TabPage4 As TabPage
     Friend WithEvents Panel1 As Panel
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents Label1 As Label
 End Class
--- drivethru/testing.vb
+++ drivethru/testing.vb
@@ -7,7 +7,7 @@
         Me.FormBorderStyle = FormBorderStyle.None
         Me.WindowState = FormWindowState.Maximized
 
-        ' Panel sebagai container
+        ' panel as container
         Panel1.AutoScroll = True
         Panel1.Dock = DockStyle.Fill
         Panel1.Padding = New Padding(0, 0, SystemInformation.VerticalScrollBarWidth, 0) ' agar scrollbar tidak menutupi isi
@@ -17,11 +17,24 @@
         TableLayoutPanel1.Dock = DockStyle.Top ' atau Fill jika ingin memenuhi panel
         TableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.AddRows
 
+        ListView1.View = View.Details
+        ListView1.Columns.Add("Item", 150, HorizontalAlignment.Left)
+        ListView1.Columns.Add("Qty", 50, HorizontalAlignment.Center)
+        ListView1.Columns.Add("Price", 70, HorizontalAlignment.Right)
+        ListView1.Columns.Add("Total", 70, HorizontalAlignment.Right)
 
 
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TabPage1_Click(sender As Object, e As EventArgs) Handles TabPage1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
 End Class
